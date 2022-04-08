c7d1c89932244ee7a6030cc0b52f06f08ca0fcc8
Alen Su <alen40817a@gmail.com>
2022-04-08 15:45:57 +0800

    update

---
+++ vue12_composition_vue3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>{{ userName }}</h3>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const userNameRef = ref("Alen Su");
+    setTimeout(function () {
+      userNameRef.value = "alen suu";
+    }, 2000);
+    return { userName: userNameRef };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
