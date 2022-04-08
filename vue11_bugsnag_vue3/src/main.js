dcde4be626e6586f56ba33a4a322ec21bb57f06a
Alen Su <alen40817a@gmail.com>
2022-04-08 15:24:37 +0800

    update

--- vue11_bugsnag_vue3/src/main.js
+++ vue11_bugsnag_vue3/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginVue from "@bugsnag/plugin-vue";
+Bugsnag.start({
+  apiKey: "314aa77f3dd5b7b6ae12b2c18bbadd7d",
+  plugins: [new BugsnagPluginVue()],
+});
+const bugsnagVue = Bugsnag.getPlugin("vue");
+createApp(App).use(bugsnagVue).mount("#app");
