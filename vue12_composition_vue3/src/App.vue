2484b493ca887d3c986e845475a3a73ffe88784c
Alen Su <alen40817a@gmail.com>
2022-04-08 17:05:06 +0800

    update

--- vue12_composition_vue3/src/App.vue
+++ vue12_composition_vue3/src/App.vue
@@ -9,10 +9,12 @@
   <h1>{{ user2 }}</h1>
   <h1>{{ user2Name }}</h1>
   <h1>{{ user2Age }}</h1>
+  <br />
+  <h1>{{ course }}</h1>
 </template>
 
 <script>
-import { ref, reactive, isRef, isReactive } from "vue";
+import { ref, reactive, isRef, isReactive, toRefs } from "vue";
 
 export default {
   setup() {
@@ -26,6 +28,12 @@ export default {
     console.log(
       `user2 is ref?${isRef(user2)}, is reactive? ${isReactive(user2)}`
     );
+    const courseRef = toRefs({ courseName: "poop", duration: 35 });
+    console.log(
+      `courseRef is ref?${isRef(courseRef)}, courseRef.duration is ref?${isRef(
+        courseRef.duration
+      )}`
+    );
     setTimeout(function () {
       userNameRef.value = "alen suu";
       ageRef.value += 1;
@@ -33,6 +41,8 @@ export default {
       userRef.value.age = 60;
       user2.userName = "dant123";
       user2.age = 40;
+      courseRef.courseName = "python and object oriented";
+      courseRef.duration = 70;
     }, 2000);
     return {
       userName: userNameRef,
@@ -43,6 +53,7 @@ export default {
       user2: user2,
       user2Name: user2.userName,
       user2Age: user2.age,
+      course: courseRef,
     };
   },
 };
---
+++ vue13_composition2_vue3/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h3>{{ course }}</h3>
+    <h3>{{ price }}</h3>
+    <button @click="extraDuration">bad way</button>
+    <h4>{{ courseDisplayName }}</h4>
+  </section>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    const course = reactive({ name: "poop", duration: 35 });
+    const price = ref(24000);
+    const hello = ref("hello");
+    const vue = ref("vue");
+    const courseDisplayName = computed(function () {
+      return hello.value + " " + vue.value;
+    });
+    function extraDuration() {
+      course.duration += 7;
+      price.value += 2000;
+    }
+
+    return { course: course, price, extraDuration, courseDisplayName };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
